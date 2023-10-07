8d6cc88bcb415aa0e4ea2062d5062610a3d85eaa
phanx <phanxuanthang20@gmail.com>
2023-10-07 22:02:57 +0700

    riverpod get api

--- lib/riverpodd/andrea/get_api.dart
+++ lib/riverpodd/andrea/get_api.dart
@@ -83,3 +83,19 @@ class Home extends StatelessWidget {
     );
   }
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Random Activity App'),
+        ),
+        body: const Home(),
+      ),
+    );
+  }
+}
